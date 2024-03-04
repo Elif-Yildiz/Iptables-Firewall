08fccc223661b256b3333be6c9407770a88eccc6
Elif-Yildiz <105632715+Elif-Yildiz@users.noreply.github.com>
2024-03-04 09:51:42 +0300

    Add files via upload

--- iptables.sh
+++ iptables.sh
@@ -87,11 +87,8 @@ sudo ip netns exec client2 ip route add default via ${fw_c2_ip} dev veth-c2
 sudo ip netns exec server ip route add default via ${fw_srv_ip} dev veth-srv
 sudo ip netns exec firewall ip route add default via ${hst_ip} dev veth-fw-hst
 
-#adding other routes(LATER)
-
-
-
-sudo route add -net 192.0.2.0 netmask 255.255.255.0 gw ${fw_hst_ip}
+#before going out for internet have nat table fixed
+sudo route add -net 192.0.2.0 netmask 255.255.255.0 gw ${fw_hst_ip}  
 sudo iptables -t nat -A POSTROUTING -s 192.0.2.0/24 -j MASQUERADE
 sudo ip netns exec firewall ip route add default via ${hst_ip} dev veth-fw-hst
 
@@ -106,28 +103,29 @@ sudo ip netns exec firewall ip route add default via ${hst_ip} dev veth-fw-hst
 # Client and server networks can be accessed to the internet from the firewall namespace via your host machine.
 
 
-sudo ip netns exec firewall iptables -Fc # flush the prev rulezzzz dummy
+sudo ip netns exec firewall iptables -F # flush the prev rulezzzz dummy
 
-# set default policies to drop. if you are not prepared for unknown ,it will drop so you will be safe ^^
+# set default policies to drop. If you are not prepared for unknown, it will drop so you will be safe ^^
 sudo ip netns exec firewall iptables -P INPUT DROP
 sudo ip netns exec firewall iptables -P OUTPUT DROP
 sudo ip netns exec firewall iptables -P FORWARD DROP
 
 # INPUT CHAIN
 
-sudo ip netns exec firewall iptables -A INPUT -p icmp -s ${c1_ip} -i veth-fw-c1 -d ${c1_fw_ip} -j DROP #REJECT #default policye değilde bu kurala takıldığının anlamak için reject yaptım, normalde drop olarak kullanacağım
+sudo ip netns exec firewall iptables -A INPUT -p icmp -s ${c1_ip} -i veth-fw-c1 -d ${c1_fw_ip} -j DROP #REJECT #Instead of defult policy, I wanted to make sure this rule works. That is why I used reject, normally I will use it as drop. # This rule satisfies "Client1 doesn't have ping permission to the firewall"
 
-sudo ip netns exec firewall iptables -A INPUT -p icmp -s ${c2_ip} -i veth-fw-c1 -d ${c2_fw_ip} -j ACCEPT
+sudo ip netns exec firewall iptables -A INPUT -p icmp -s ${c2_ip} -i veth-fw-c1 -d ${c2_fw_ip} -j ACCEPT # Client2 can ping the firewall
 
-sudo ip netns exec firewall iptables -A INPUT -i lo -j ACCEPT # enable all firewall's trafic baby, no red lights
+# enable all firewall's trafic baby, no red lights
+sudo ip netns exec firewall iptables -A INPUT -i lo -j ACCEPT 
 sudo ip netns exec firewall iptables -A INPUT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 sudo ip netns exec firewall iptables -A INPUT -p udp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 sudo ip netns exec firewall iptables -A INPUT -i veth-fw-hst -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 sudo ip netns exec firewall iptables -A INPUT -i veth-fw-hst -p tcp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
 # FORWARD CHAIN
-sudo ip netns exec firewall iptables -A FORWARD -s ${c2}${sbn} -d ${srv}${sbn} -i veth-fw-c2 -o veth-fw-srv -p tcp --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-sudo ip netns exec firewall iptables -A FORWARD -s ${srv}${sbn} -d ${c2}${sbn} -i veth-fw-srv -o veth-fw-c2 -p tcp --sport 80 -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo ip netns exec firewall iptables -A FORWARD -s ${c2}${sbn} -d ${srv}${sbn} -i veth-fw-c2 -o veth-fw-srv -p tcp --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT #Client2 can access the server for HTTP
+sudo ip netns exec firewall iptables -A FORWARD -s ${srv}${sbn} -d ${c2}${sbn} -i veth-fw-srv -o veth-fw-c2 -p tcp --sport 80 -m state --state ESTABLISHED,RELATED -j ACCEPT 
 
 sudo ip netns exec firewall iptables -A FORWARD -i veth-fw-c1 -o veth-fw-hst -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT # packets coming from client1 are enabled
 sudo ip netns exec firewall iptables -A FORWARD -o veth-fw-c1 -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT # packets going out from client1 are enabled
@@ -140,6 +138,7 @@ sudo ip netns exec firewall iptables -A FORWARD -o veth-fw-srv -p icmp -m state 
 
 
 # OUTPUT CHAIN
+# enable firewalls trafic that is going out, no red lightzzz baby
 sudo ip netns exec firewall iptables -A OUTPUT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 sudo ip netns exec firewall iptables -A OUTPUT -o lo -j ACCEPT
 sudo ip netns exec firewall iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
@@ -148,7 +147,7 @@ sudo ip netns exec firewall iptables -A OUTPUT -o veth-hst -p tcp --dport 80 -m 
 sudo ip netns exec firewall iptables -A OUTPUT -o veth-fw-hst -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 sudo ip netns exec firewall iptables -A OUTPUT -o veth-fw-hst -p tcp --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
-# iptables -I INPUT -p tcp --dport 22 -j REJECT
+# iptables -I INPUT -p tcp --dport 22 -j REJECT # this part is for a memory of ssh wars we had, note: close the ports you do not use
 
 
 
